d70994a5fc7ff3e3e670aa80598e3390b9eccc80
Rapd33 <rafaelpatinodiaz@gmail.com>
2025-12-29 20:31:15 -0500

    Implementar estructura principal de la aplicación con enrutamiento y componentes
    Creado AppComponent como punto de entrada principal de la aplicación.
    Añadida configuración de rutas para los componentes de validación, registro y dashboard.
    
    Desarrollado ValidationComponent para la validación de ID de cliente con rutas hacia registro o dashboard.
    
    Implementado RegisterComponent para el registro de nuevos clientes.
    
    Desarrollado DashboardComponent para mostrar el análisis de riesgo del cliente.
    
    Incorporado CustomerService para gestionar interacciones con la API y datos simulados.
    
    Añadidos modelos para Customer y ChurnPrediction.
    
    Creadas pruebas unitarias para los componentes para asegurar su funcionalidad.
    
    Estilizada la aplicación con CSS global para un aspecto moderno.

---
+++ FrontEnd/src/app/components/dashboard/cliente-nuevo-form/cliente-nuevo-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-nuevo-form',
+  imports: [],
+  templateUrl: './cliente-nuevo-form.html',
+  styleUrl: './cliente-nuevo-form.css',
+})
+export class ClienteNuevoForm {
+
+}
---
+++ FrontEnd/src/app/components/dashboard/dashboard-general/dashboard-general.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-general',
+  imports: [],
+  templateUrl: './dashboard-general.html',
+  styleUrl: './dashboard-general.css',
+})
+export class DashboardGeneral {
+
+}
